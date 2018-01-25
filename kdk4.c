b49073707f7cd8fe419739b0450f48c970cccae6
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-01-25 16:50:12 +0530

    Create kdk4.c

---
+++ kdk4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int year;
+printf("\n enter the year");
+scanf("%d",&year);
+if (year%2==0)
+printf("%d is leap year",year);
+else
+printf("%d is not aleap year", year);
+}
