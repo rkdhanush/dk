0ba7579209fb5d351e9de593bc4559a9d3dccf94
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-01-26 05:01:22 +0000

    Create sum of first three.c

---
+++ sum of first three.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+voidmain()
+{
+int i,N[i],c=0,T,K;
+printf("get sum upto no. of integers");
+scanf("%d%d",&K,&T);
+for(i=1;i<=T;i++);
+{
+scanf("%d",&N[i]);
+}
+for(i=1;i<=K;i++);
+{
+c=c+N[i];
+}
+printf(" %d is sum",c);
+}
