7f2cd5e90011ca734472ba7812fbe46d7132d1c5
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-02-06 15:52:22 +0530

    Create npower.c

---
+++ npower.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the base number");
+scanf("%d",&a);
+printf("enter the power number");
+scanf("%d",&b);
+c=pow(a,b);
+printf("%d",c);
+}
