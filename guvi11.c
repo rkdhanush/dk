d13083d884b4f7889a87965124d84b5642b9bd50
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-02-20 16:41:23 +0530

    Update guvi11.c

--- guvi11.c
+++ guvi11.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a[50],h,i,m;
+int a[50],j,i,m;
 printf("enter the size of arr");
-scanf("%d",&h);
+scanf("%d",&j);
 printf("enter the array elements");
-for(i=0;i<h;i++)
+for(i=0;i<j;i++)
 {
 scanf("%d",&a[i]);
 }
 m=a[0];
-for(i=1;i<h;i++)
+for(i=1;i<j;i++)
 {
 if(m>a[i])
 m=a[i];
