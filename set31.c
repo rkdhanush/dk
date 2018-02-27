dfdfb6c3fdd0d6783db2b09656070ef4e49e3dec
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-02-27 16:41:54 +0530

    Create set31.c

---
+++ set31.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char str[50];
+    int i,count=0;
+    printf("enter the sentence");
+    scanf("%[^n\]s",str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        count=count+1;
+    }
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        {
+            count=count-1;
+        }
+    }
+    printf("\n ans is:%d",count);
+    return 0;
+}
