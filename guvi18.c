eb3584ce5003164f24e3407794d4ff085305023a
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-02-27 15:53:02 +0530

    Create guvi18.c

---
+++ guvi18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int array[10],n,j;
+printf("enter the size of the azrray");
+scanf("%d",&n);
+printf("\nenter the array elements");
+for(j=0;j,n;j++)
+{
+scanf("%d",&array[j]);
+printf(\n%d %d,array[j],j);
+}
+return 0;
+}
