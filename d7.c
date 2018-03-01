eac669425130cb4c062fa466c0dabe2d5651dbf8
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-03-01 16:52:06 +0530

    Create d7.c

---
+++ d7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+int d,k;
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&d,&k);
+printf("\n before swapping:");
+printf("\nx=%d",d);
+printf("\ny=%d",k);
+d=d^k;
+k=d^k;
+d=d^k;
+printf("\n after swapping:");
+printf("\nd=%d",d);
+printf("\nk=%d",k);
+return 0;
+}
