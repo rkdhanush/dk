4e27a6bac02776fd037cb1b6decbf0a549e1b6f2
rkdhanush <35653106+rkdhanush@users.noreply.github.com>
2018-03-02 09:20:03 +0000

    Update a9.c

--- a9.c
+++ a9.c
@@ -2,17 +2,17 @@
 
 int main(void) 
 {
-int a=1,b=1,i,n,c;
+int a=1,b=1,i,n,d;
 printf("\n enter the range to print the Fibonacci numbers\n");
 scanf("%d",&n);
 printf("%d",a);
 printf("\t%d",b);
 for(i=1;i<=n-2;i++)
 {
-c=a+b;
-printf("\t%d",c);
+d=a+b;
+printf("\t%d",d);
 a=b;
-b=c;
+b=d;
 }
 return 0;
 }
